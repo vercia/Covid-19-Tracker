5f66b5396b13011757f6d317e1948c4a65f68084
Weronika <werawera112@gmail.com>
2020-05-07 00:47:16 +0200

    create styles App and edit App file - change from function component to class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api'
 
-function App() {
-  return (
-    <div>
-      <h1>App</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    data: {}
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData })
+  }
+
+  render() {
+    const { data } = this.state;
+    
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
 }
 
 export default App;
