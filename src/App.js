abe17e35daa115ea054b2491a606345a2b8b9127
Weronika <werawera112@gmail.com>
2020-05-08 00:25:08 +0200

    import fetchCountries to CountryPicker and map with option; add function handleCountryChange

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
-import { fetchData } from './api'
+import { fetchData } from './api';
 
 class App extends React.Component {
   state = {
-    data: {}
-  }
+    data: {},
+    country: ''
+  };
 
   async componentDidMount() {
     const fetchedData = await fetchData();
-    this.setState({ data: fetchedData })
+    this.setState({ data: fetchedData });
   }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    console.log(fetchedData)
+    //set the state
+  };
 
   render() {
     const { data } = this.state;
-    
+
     return (
       <div className={styles.container}>
         <Cards data={data} />
-        <CountryPicker />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart />
       </div>
     );
